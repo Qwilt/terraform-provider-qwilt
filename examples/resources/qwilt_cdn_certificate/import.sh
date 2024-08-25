a19641f8c158bb46c89563ac7313a9ed177617af
estherb <estherb@qwilt.com>
2024-08-25 12:50:51 +0300

    testing the git workflow. Removed the esther test comment from the Import section of the qwilt_certificate resource.

--- examples/resources/qwilt_cdn_certificate/import.sh
+++ examples/resources/qwilt_cdn_certificate/import.sh
@@ -1,5 +1,5 @@
 
-#keep an empty resource to import into (Test Esther Delete This)
+#keep an empty resource to import into
 #after import is completed the user should manually set the required attributes in the resource from the imported state file
 resource "qwilt_cdn_certificate" "example" {
 
